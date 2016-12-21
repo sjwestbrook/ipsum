5c12f770cb9bfb5c7e77320fc0e1be9a3e608c9d
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-12-21 14:49:12 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
+var routerApp = angular.module('routerApp', ['ui.router']);
 
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATE ========================================
+//         .state('home', {
+//             url: '/home',
+//             templateUrl: './partials/home.html'
+//         })           
+    
+        .state('home', {
+            url: '/home',
+            views: {
+                '': { 
+                    templateUrl: './partials/home.html' 
+                },
+                'ipsum@ipsum': { 
+                    templateUrl: './partials/ipsum.html',
+                    controller: 'ipsumController'
+                }
+            }
+        });
+    
+});
