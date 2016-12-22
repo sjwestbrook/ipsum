22cb75d112576af7150d69fdabee4c1009208e45
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-12-21 17:31:26 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
+var routerApp = angular.module('routerApp', ['ui.router']);
 
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATE ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: './partials/home.html'
+        })
+        
+     
+      
+    
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('home', {
+            url: '/home',
+            views: {
+                '': { templateUrl: './partials/home.html' },
+                'columnLeft@home': { template: 'Look I am a column!' },
+                'columnRight@home': { 
+                    templateUrl: './partials/ipsum.html',
+                    controller: 'ipsumController'
+                }
+            }            
+        });
+  
+});
