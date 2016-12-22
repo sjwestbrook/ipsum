f84a011ae549510cbd41076b40a7b85e2a2f025d
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-12-21 17:59:50 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,21 +2,18 @@ var routerApp = angular.module('routerApp', ['ui.router']);
 
 routerApp.config(function($stateProvider, $urlRouterProvider) {
     
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/ipsum');
     
     $stateProvider
             
-        // HOME PAGE AND NESTED NAMED VIEWS =================================
-        .state('home', {
-            url: '/home',
-            views: {
-                '': { templateUrl: './partials/home.html' },
-//                 'columnRight@home': { template: 'Look I am a column!' },
-                'columnLeft@home': { 
-                    templateUrl: './partials/ipsum.html',
-                    controller: 'ipsumController'
-                }
-            }            
-        });
+    // HOME PAGE AND NESTED NAMED VIEWS =================================
+    .state('home', {
+        url: '/index',
+        'columnLeft@home': { 
+            templateUrl: './partials/ipsum.html',
+            controller: 'ipsumController'
+        }
+
+    });
   
 });
